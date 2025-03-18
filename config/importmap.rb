71a08fb486239dc8d920db598defecbcc89f38a0
Aron Emilsson <aron_hannes@hotmail.com>
2025-03-18 22:04:46 +0100

    jæja

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,6 +8,7 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.2
 pin "three", to: "https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js"
 pin "three/examples/jsm/loaders/GLTFLoader", to: "https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/loaders/GLTFLoader.js"
+
 pin_all_from "app/javascript/controllers", under: "controllers"
 # pin "gsap" # @3.12.7
 # pin "gsap/ScrollTrigger", to: "gsap--ScrollTrigger.js" # @3.12.7
--- db/migrate/20250203125835_create_videos.rb
+++ db/migrate/20250203125835_create_videos.rb
@@ -8,3 +8,13 @@ class CreateVideos < ActiveRecord::Migration[7.1]
     end
   end
 end
+
+
+# 1. Open the Rails console by running the following command in your terminal:
+
+# ----> "rails console " in the terminal
+
+# 2. Create a new video object by running the following command in the Rails console:
+
+# -----> video = Video.new(title: "My Video Title", youtube_url: "https://www.youtube.com/watch?v=example_id")
+# video.save
