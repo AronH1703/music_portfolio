3d4b356fdef8c9cb0380fc6a65677392cceb5a8a
Aron Emilsson <aron_hannes@hotmail.com>
2025-10-18 14:12:44 +0200

    seeds fixed

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,52 @@
-class Video < ApplicationRecord
+require 'uri'
+require 'cgi'
 
+class Video < ApplicationRecord
+  # Build a robust YouTube embed URL from various input formats:
+  # - https://www.youtube.com/watch?v=VIDEO_ID[&...]
+  # - https://youtu.be/VIDEO_ID[?t=...]
+  # - https://www.youtube.com/embed/VIDEO_ID
+  # - https://www.youtube.com/shorts/VIDEO_ID
   def youtube_embed_url
-    youtube_id = youtube_url.split("v=").last
-    "https://www.youtube.com/embed/#{youtube_id}"
+    id = extract_youtube_id(youtube_url)
+    return "" if id.nil?
+    "https://www.youtube.com/embed/#{id}"
+  end
+
+  private
+
+  def extract_youtube_id(url)
+    return nil if url.blank?
+
+    begin
+      uri = URI.parse(url)
+    rescue URI::InvalidURIError
+      return nil
+    end
+
+    host = uri.host.to_s
+    path = uri.path.to_s
+
+    id = nil
+
+    if host.include?("youtu.be")
+      # youtu.be/VIDEO_ID
+      id = path.split("/").reject(&:blank?).first
+    elsif host.include?("youtube.com")
+      if path.start_with?("/embed/")
+        # youtube.com/embed/VIDEO_ID
+        id = path.split("/")[2]
+      elsif path.start_with?("/shorts/")
+        # youtube.com/shorts/VIDEO_ID
+        id = path.split("/")[2]
+      elsif path == "/watch"
+        # youtube.com/watch?v=VIDEO_ID
+        params = CGI.parse(uri.query.to_s)
+        id = Array(params["v"]).first
+      end
+    end
+
+    # Clean up any accidental trailing characters
+    id&.to_s&.gsub(/[^0-9A-Za-z_-]/, "")
   end
 end
